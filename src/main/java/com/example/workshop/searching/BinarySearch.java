d7a7d6fbd5e92ad94f39f43ebbe31887df919f2f
Shrutime2706 <shrutisri267@gmail.com>
2022-07-09 14:00:20 +0530

    done binary search

--- src/main/java/com/example/workshop/searching/BinarySearch.java
+++ src/main/java/com/example/workshop/searching/BinarySearch.java
@@ -4,15 +4,18 @@ public class BinarySearch {
 	public static void main(String args[])
 	{
 		int a[]= {1,3,5,7,9};
-		int target=1;
+		int target=9;
 		int s=0;
 		int l=a.length-1;
-		int mid=(s+l)/2;
+		
 		while(s<=l)
+			
 		{
+			int mid=(s+l)/2;
 			if(a[mid]==target)
 			{
 			System.out.print(mid);
+			break;
 			}
 			else if(a[mid]>target)
 			{
